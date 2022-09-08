fbe7808c9b38fd8bfc2b8f2ced34642c95f7e350
efodthe2nd <davidefod@gmail.com>
2022-09-08 18:01:55 +0000

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[15];
+	char str2[54];
+
+	strcpy(str2, "\"Programming is like building a multilingual puzzle \n");
+
+	puts(str2);
+
+	return(0);
+}
+
+
