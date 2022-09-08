81d656ed03ce68f41f7034f4cb1cf1c311f0fb86
efodthe2nd <davidefod@gmail.com>
2022-09-08 21:07:32 +0000

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+	int c;
+	long d;
+	int e;
+	float f;
+	char b[] = "byte(s)";
+
+	printf("Size of a char: %lu %s\n", sizeof(i), b);
+	printf("Size of an int: %lu %s\n", sizeof(c), b);
+	printf("Size of a long int: %lu %s\n", (long)sizeof(d), b);
+	printf("Size of a long long int: %lu %s\n", (unsigned long)sizeof(e), b);
+	printf("Size of a float: %lu %s\n", sizeof(f), b);
+	return (0);
+}
