e2c8c3d25b0ffcdd3e252974fc27165e36bc28be
efodthe2nd <davidefod@gmail.com>
2022-09-08 18:25:50 +0000

    remove spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,12 @@
 
 /**
  *  main - prints out string
- *  Return: 0 if success 
+ *  Return: 0 if success. 
  */
 int main(void)
 {
 	char str2[52];
-
 	strcpy(str2, "\"Programming is like building a multilingual puzzle");
-
 	puts(str2);
 	return (0);
 }
