3532d49a4d54fa41586ac0039278e852df390cc8
efodthe2nd <davidefod@gmail.com>
2022-09-09 21:26:09 +0000

    change ch to char

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
 
 int main(void)
 {
-	char ch;
+	int ch;
 
-	for (ch = "a"; ch <= "z"; ch++)
+	for (ch = 97; ch <= 122; ++ch)
 	{
 		putchar(ch);
 	}
-	for (ch = "A"; ch <= "Z"; ch++)
+	for (ch = 65; ch <= 90; ++ch)
 	{
 		putchar(ch);
 	}
