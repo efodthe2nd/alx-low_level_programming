c05da6fb2d494eeaebf2ccec03751ac0881ec830
efodthe2nd <davidefod@gmail.com>
2022-09-13 19:08:33 +0000

    add quotes in char

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 {
 	int i,j,k,l;
 
-	for (i = 0; i < 3; i++)
+	for (i = '0'; i < '3'; i++)
 	{	
-		for (j = 0; j < 4; j++)
+		for (j = '0'; j < '4'; j++)
 		{	
 			
-			for (k = 0; k < 6; k++)
+			for (k = '0'; k < '6'; k++)
 			{	
-				for (l = 0; l < 10; l++)
+				for (l = '0'; l < '10'; l++)
 				{
 					_putchar(i);
 					_putchar(j);
