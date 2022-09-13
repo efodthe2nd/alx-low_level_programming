2b74dd4dfce7264d1b3b1c14aea9b36b12d68a44
efodthe2nd <davidefod@gmail.com>
2022-09-13 18:25:24 +0000

    7s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,20 @@
 
 /**
  * print_last_digit - prints value of the last digit
- * n: character to be checked
+ * @n: character to be checked
  * Return: n if success
  */
 
 int print_last_digit(int n)
 {
 	n = n % 10;
-	
-	if (n < 10)
+
+	if (n < 0)
 	{
 		n = -n;
 		_putchar(n + '0');
+
+		return (n);
 	}
 
 	return (n);
