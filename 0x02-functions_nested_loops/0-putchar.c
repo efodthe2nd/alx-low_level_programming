d65c32a1295b1f10807c1cc232ac6463829b463c
efodthe2nd <davidefod@gmail.com>
2022-09-13 14:32:48 +0000

    Third try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,16 @@
 
 int main(void)
 {
-	char c = 'A';
-	_putchar(c);
+	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,4 @@
 
 
-#include <unistd.h>
-
-int _putchar(char c)
+int _putchar(char c);
   
