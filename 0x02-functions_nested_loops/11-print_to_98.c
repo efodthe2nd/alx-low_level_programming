63477407fe236d22d1809a94792153283ec87d73
efodthe2nd <davidefod@gmail.com>
2022-09-13 22:42:13 +0000

    2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,10 @@
 
 void print_to_98(int n)
 {
-
-	if (n > 98)
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	} else if (n > 98)
 	{
 		while (n >= 99)
 		{
@@ -19,19 +21,14 @@ void print_to_98(int n)
 			n--;
 		}
 		printf("%d\n", 98);
-	}
-	if (n < 98)
+	} else if (n < 98)
 	{
 		while (n <= 97)
 		{
 			printf("%d, ", n);
 			n++;
 		
-		printf("--------");}
+		}
 		printf("%d\n", 98);
-	}
-	if (n == 98)
-	{
-		printf("%d\n", n);
 	}
 }
