2057c7a9f6b59568a91986f3cd8d6510400029e3
efodthe2nd <davidefod@gmail.com>
2022-09-13 19:13:13 +0000

    fix whitespace

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@
 
 void jack_bauer(void)
 {
-	int i,j,k,l;
+	int i, j, k, l;
 
 	for (i = 48; i < 51; i++)
-	{	
+	{
 		for (j = 48; j < 52; j++)
-		{	
-			
+		{
+
 			for (k = 48; k < 54; k++)
-			{	
+			{
 				for (l = 48; l <= 57; l++)
 				{
 					_putchar(i);
