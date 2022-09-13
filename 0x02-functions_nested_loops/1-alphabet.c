1100b5194f0b399ec9ff9107be066ee61eeeea81
efodthe2nd <davidefod@gmail.com>
2022-09-13 15:00:23 +0000

    add print_alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,18 @@
 
 int main(void)
 {
+	print_alphabet();
+
+	return (0);
+}
+
+void print_alphabet(void)
+{
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		print_alphabet();
+		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
