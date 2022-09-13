175e03daee73d67917354eb5880f649f2f779df1
efodthe2nd <davidefod@gmail.com>
2022-09-13 17:47:37 +0000

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value of int
+ * @int: value returned
+ *
+ * Return: 0 if success.
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = int * -1;
+		return (int);
+	}
+	return (int);
+}
