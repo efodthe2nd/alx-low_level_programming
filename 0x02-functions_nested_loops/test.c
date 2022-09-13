8e569fd70b933f8f0d146d5e89d27fb06a155170
efodthe2nd <davidefod@gmail.com>
2022-09-13 20:59:01 +0000

    main

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * return: 0 if success
+ * jack_bauer - annohyance
+ * Returns: 0 if success
  */
 
 int main(void)
 {
-	int i,j,k,l;
-	for (i = 0; i < 3; i++)
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 10; hr++)
 	{
-		for (j = 0; j < 4; j++)
-		{	
+		for (min = 0; min < 10; min++)
+		{
+			printf("%d", hr * min);
 			
-			for (k = 0; k < 6; k++)
-			{	
-				for (l = 0; l < 10; l++)
-				{
-					printf("%d",i);
-					printf("%d",j);
-					printf(":");
-					printf("%d",k);
-					printf("%d",l);
-					printf("\n");
-				}
-			}
 		}
+		printf("\n");
 	}
-
-		return (0);
-
+	return (0);
 }
