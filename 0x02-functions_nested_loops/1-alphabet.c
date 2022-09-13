98e415601cb73eb99837185a107c397913120eed
efodthe2nd <davidefod@gmail.com>
2022-09-13 14:44:02 +0000

    2-first try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Main entry function
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 
 
 int _putchar(char c);
+void print_alphabet(void);
   
