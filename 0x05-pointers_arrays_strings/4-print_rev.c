bd79d9d0fd49f2989f221495dbb32c214e18cb25
efodthe2nd <davidefod@gmail.com>
2022-09-19 23:17:21 +0000

    I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: character to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
