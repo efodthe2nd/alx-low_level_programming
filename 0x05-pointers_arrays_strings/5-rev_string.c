8bbf4c7b25996441590152fc392e27837e97f690
efodthe2nd <davidefod@gmail.com>
2022-09-19 23:41:06 +0000

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: character to be printed
+ */
+
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		i++;
+	}
+
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
