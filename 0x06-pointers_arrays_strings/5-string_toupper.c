8fe93b89a3ca1216ab7968dc330f196a667dcde3
efodthe2nd <davidefod@gmail.com>
2022-09-22 21:03:57 +0000

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#includes "main.h"
+
+/**
+ * string_toupper - changes all lowercase
+ * @s: string to change
+ *
+ * Return: address of s
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) = 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
