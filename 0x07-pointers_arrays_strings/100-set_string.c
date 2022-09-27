2b0365fe654f0d714b755ee3510e3469b0f181f2
efodthe2nd <davidefod@gmail.com>
2022-09-27 22:04:48 +0000

    100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets the calue of pointer
+ * @s: first string
+ * @to: second string
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
