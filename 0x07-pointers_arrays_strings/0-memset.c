84906bcee83c18ee8dc5c12125ac1fea89e43e8b
efodthe2nd <davidefod@gmail.com>
2022-09-27 10:05:46 +0000

    Initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_memset - fills n bytes of memory
+ * @*s: pointer variable
+ * @b: constant byte variable
+ * @n: number of bytes of memory
+ *
+ * Return: char on success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
