9ffb2a23329e06f8faed060b5fa1fc1e7acce607
efodthe2nd <davidefod@gmail.com>
2022-09-28 20:55:36 +0000

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * sqrt2 - Makes possible to evaluate
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ *
+ * Return: on success 1.
+ */
+
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root
+ * @n: Number integer
+ *
+ * Return: On success 1.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
