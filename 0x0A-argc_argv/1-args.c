8a77b0e65b172a633c0047cd4df2c936e6c5bd8f
efodthe2nd <davidefod@gmail.com>
2022-10-01 20:35:30 +0000

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints argc count
+ *@argc: argument count
+ @argv: argument vector
+ * Return: 0 if succes
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
