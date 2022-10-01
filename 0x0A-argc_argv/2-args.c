a484710f31c233286b0d07945be104c7173aae55
efodthe2nd <davidefod@gmail.com>
2022-10-01 21:42:27 +0000

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: count int
+ * @argv: integers available
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
