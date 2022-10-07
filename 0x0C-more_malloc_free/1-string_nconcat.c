e7d401f3292c4cfef3ee868143f0cdfb153608e6
efodthe2nd <davidefod@gmail.com>
2022-10-07 14:41:59 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,10 +29,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[i])
 		i++;
-	
+
 	while (s2[k])
 		k++;
-	
+
 	if (n >= k)
 		l = i + k;
 	else
@@ -54,6 +54,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		j++;
 	}
-	str[j] = '\0';
+	p[j] = '\0';
 	return (p);
 }
