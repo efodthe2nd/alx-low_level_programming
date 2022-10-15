6e85c7de812756377d31f9e565029c76b4d5a928
efodthe2nd <davidefod@gmail.com>
2022-10-15 01:26:56 +0000

    4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define SUM(x, y) (x) + (y)
+
--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,13 +1,11 @@
-#include "3-function_like_macro.h"
+#include "4-sum.h"
 #include <stdio.h>
 
 int main(void)
 {
-	 int i;
-	 int j;
+	 int s;
 
-	 i = ABS(-98) * 10;
-	 j = ABS(98) * 10;
-	 printf("%d, %d\n", i, j);
+	 s = SUM(98, 1024);
+	 printf("%d\n", s);
 	 return (0);
 }
