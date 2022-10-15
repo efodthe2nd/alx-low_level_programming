11e02247e71a28a8681812d549d4d575ed5a9852
efodthe2nd <davidefod@gmail.com>
2022-10-15 03:32:39 +0000

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - a dog wey dey craze
+ * @name: name of dog
+ * @age: how old the dog is
+ * @owner: present owner
+ *
+ * Description: This is a struct for a doggie
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
