5e2a0e158d0acaf6d5466227f221ce4504c7c4c6
efodthe2nd <davidefod@gmail.com>
2022-10-16 15:46:51 +0000

    3sp

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -51,7 +51,7 @@ void print_all(const char * const format, ...)
  */
 void print_a_char(char *separator, va_list args)
 {
-	printf("%s%c", separator, va_args(args, int));
+	printf("%s%c", separator, va_arg(args, int));
 }
 
 /**
@@ -75,7 +75,7 @@ void print_a_integer(char *separator, va_list args)
  * Return: Nothing
  */
 
-void print_a_float(Char *separator, va_list args)
+void print_a_float(char *separator, va_list args)
 {
 	printf("%s%f", separator, va_arg(args, double));
 }
