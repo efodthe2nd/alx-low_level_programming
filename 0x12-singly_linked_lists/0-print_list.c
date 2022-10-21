26e9182580ba2af0336df2dfef8e40631f2fe540
efodthe2nd <davidefod@gmail.com>
2022-10-21 16:13:15 +0000

    2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,15 +18,15 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 		{
-			printf("[%d]", h->len);
-			printf(" %s \n", h->str);
+			printf("[0] (nil)\n");
 		}
 		else
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
+
 		count++;
 	}
 
