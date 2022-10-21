94900e38f16e2bffdc95195788596c14b9f03629
efodthe2nd <davidefod@gmail.com>
2022-10-21 22:19:49 +0000

    100 finally

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void first(void) __attribute__((constructor));
  * Return: nothing
  */
 
-void first(void) 
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
