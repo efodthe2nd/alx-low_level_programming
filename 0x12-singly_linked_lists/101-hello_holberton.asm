78bb8dbba2ab0dd4cfcebb9a6a7143be1b75767a
efodthe2nd <davidefod@gmail.com>
2022-10-21 22:34:09 +0000

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,30 +1,17 @@
-;"Hello, world" in assembly language for Linux
-;
-;to build an executable:
-;	nasm -f elf hello.asm
-;	ld -s -0 hello hello.o
-
 section .text
-  global_start
+global main
 
-section .data
-msg db 'Hello, Holberton', 10 ;our dear string
-len equ $ -msg
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
 
-section .text
+	mov rax, 60
+	mov rdi, 0
+	syscall
 
-_start;
-
-;write the string to stdout:
-
-  mov edx,len
-  mov ecx,msg
-  mov ebx,1
-  mov eax,4
-  int 0x80
-
-;Exit via the kernel
-
-  mov ebx,0
-  mov eax,1
-  int 0x80
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
