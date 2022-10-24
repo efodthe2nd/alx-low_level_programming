5d164f7e8e6e4af9ace5eda1073de78d41bfa5fe
efodthe2nd <davidefod@gmail.com>
2022-10-24 20:01:50 +0000

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,15 +13,14 @@ size_t print_listint(const listint_t *h)
 	int count;
 
 	count = 0;
-	if (h  == NULL)
-		return (1);
-
-	while (h != NULL)
+	if (h  != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
-
 	return (count);
 }
