2e4e585a7d8769a552d010608f81c328266e4a49
efodthe2nd <davidefod@gmail.com>
2022-10-24 06:03:07 +0000

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a node
+ * @h: argument node
+ *
+ * Return: an int
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	if (h  == NULL)
+		return (1);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
