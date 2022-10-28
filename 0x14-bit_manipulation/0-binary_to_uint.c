5b51333d777fad468995329565fcb742a4989aae
efodthe2nd <davidefod@gmail.com>
2022-10-28 07:47:34 +0000

    Fix 0 error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,20 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+
+
+	while (b[i])
 	{
-		val <<= 1;
-		val += b[i] - '0';
+		if (b[i] == '0' || b[i] == '1')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+
 		i++;
 	}
 
