02c7758507ff456f252be14c0f5b7d8313de6719
efodthe2nd <davidefod@gmail.com>
2022-11-01 21:23:46 +0000

    1sd

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
