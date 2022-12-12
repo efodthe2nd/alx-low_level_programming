1ce6efd547c39d747170955e1b2b2d49b7f8acfe
efodthe2nd <davidefod@gmail.com>
2022-12-12 12:35:30 +0000

    initial commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - prints all the elements of a list
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n ", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
