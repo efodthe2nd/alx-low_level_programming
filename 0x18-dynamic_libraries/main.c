80936e997f6086c47bfdfa32f160b37d9091ad83
efodthe2nd <davidefod@gmail.com>
2022-12-13 00:49:03 +0000

    1s

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,8 +1,10 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 int main(void)
 {
-	_puts("\"At the end of the day\"\n\t - David Efod");
+	//_puts("\"At the end of the day\"\n\t - David Efod");
+	int name;
+	name = mul(2, 3);
+	printf("%d\n", name);
 	return (0);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
