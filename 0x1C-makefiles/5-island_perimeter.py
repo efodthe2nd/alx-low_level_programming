c0bf02c97f2400731ded5382cd464185c85bc91a
efodthe2nd <davidefod@gmail.com>
2023-02-23 15:40:35 +0000

    4

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter module
+
+contains a function that returns perimeter of an island
+"""
+
+def island_perimeter(grid):
+	"""computers perimeter of an island described in a grid
+
+	Args:
+		grid(list): list of list of integers
+	"""
+	perimeter = 0
+	height = len(grid)
+	if height == 0:
+		return perimeter
+	
+	width = len(grid[0])
+	for i in range(height):
+		for j in range(width):
+			if grid[i][j] == 1:
+				sides = [
+					grid[i - 1][j] if i != 0 else 0,
+					grid[i][j + 1] if j < width - 1 else 0,
+					grid[i + 1][j] if i < height - 1 else 0,
+					grid[i][j - 1] if j != 0 else 0,
+				]
+				perimeter += sum([1 if v == 0 else 0 for v in sides])
+	
+	return perimeter
