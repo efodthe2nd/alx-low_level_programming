3eb07697ee7adb96e831ed4037f8debf7771f369
David Efod <davidefod@gmail.com>
2023-04-11 16:05:49 +0000

    6

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,62 @@
+#include "search_algos.h"
+
+/**
+ * _binary_search - Searches for a value in a sorted array
+ * @array: pointer to the array
+ * @left: starting index of the array
+ * @right: the ending index of the array
+ * @value: value to search for
+ *
+ * Return: The value or -1.
+ */
+int _binary_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (right >= left)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
+
+/**
+ * exponential_search - Searches for a vlue in a sorted array
+ * @array; pointer to the array
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: value or -1
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 0, right;
+
+	if (array == NULL)
+		return (-1);
+
+	if (array[0] != value)
+	{
+		for (i = 1; i < size && array[i] <= value; i = i * 2)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+
+	right = i < size ? i : size - 1;
+	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
+	return (_binary_search(array, i / 2, right, value));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,15 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 
 int add(int x, int y);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif
