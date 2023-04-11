37d120ca45862e5947ca204b8e9c115709788883
David Efod <davidefod@gmail.com>
2023-04-11 16:35:27 +0000

    105

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,41 @@
 #include <stddef.h>
 #include <math.h>
 
-int add(int x, int y);
+/**
+ * @n: integer
+ * @index: Index of the node
+ * @next: pointer to the node
+ * Description: singly linked list node
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly lined list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: pointer to the node
+ * @express: pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
