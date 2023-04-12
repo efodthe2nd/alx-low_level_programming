442a4c2431bf82a5bbd6e3c422f24a1f8ded8852
David Efod <davidefod@gmail.com>
2023-04-12 08:57:31 +0000

    104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -41,6 +41,6 @@ int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
 		return (-1);
-	
+
 	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
